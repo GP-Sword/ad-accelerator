9c5c9bd0baa8fb710e011b51dd098db41a6535b2
GP-Sword <138867119+GP-Sword@users.noreply.github.com>
2025-10-22 16:41:50 +0700

    add reload page
    Reload page when encounter the ad blocker reinforcement screen

--- content_youtube.js
+++ content_youtube.js
@@ -9,30 +9,10 @@
         '[id^="skip-ad"] button',
         '[id^="skip-button"]',
     ];
-    const blockerPopup = document.querySelector("ytd-popup-container");
 
-    // Checks for ads and manipulates the video or uses skip button if present
-    function handleVideoAd() {
-        const adContainer = document.querySelector('.ad-showing');
-        if (!adContainer) {
-            return; // No ad is showing, so we do nothing.
-        }
-
-        const adVideo = adContainer.querySelector('video'); 
-        if (adVideo && adVideo.duration) {
-            muteAndSpeedUp(adVideo, 8.0);
-            try {
-                adVideo.currentTime = adVideo.duration;
-            } catch (e) {
-                // Uncaught TypeError, doesn't really change anything.
-            }
-        }
-
-        // const skipButton = document.querySelector(skipButtonSelectors.join(', '));
-        // if (skipButton) {
-        //     skipButton.click();
-        //     // console.log('Used Button to Skip Ad');
-        // }
+    function checkAdBlockerPopup() {
+        // Adblocker popup container
+        const blockerPopup = document.querySelector("ytd-popup-container");
 
         // Check and remove ad blocker popup
         if (blockerPopup) {
@@ -40,8 +20,51 @@
         }
     }
 
-    function getRandomDelay(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+    function checkEnforcementMessage() {
+        // Enforcement element that block video playback
+        const enforcementMessageElement = document.querySelector(
+            ".ytd-enforcement-message-view-model",
+        );
+        const playabilityErrorElement = document.querySelector(
+            ".yt-playability-error-supported-renderers",
+        );
+
+        // Reload page if video playback is blocked
+        if (enforcementMessageElement && playabilityErrorElement) {
+            enforcementMessageElement.remove();
+            playabilityErrorElement.remove();
+
+            window.location.reload();
+        }
+    }
+
+    // Checks for ads and manipulates the video or uses skip button if present
+    function handleVideoAd() {
+        // Deal with ad blocker elements first
+        checkAdBlockerPopup();
+        checkEnforcementMessage();
+
+        const adContainer = document.querySelector('.ad-showing');
+        if (!adContainer) {
+            return; // No ad is showing, so we do nothing
+        }
+
+        const adVideo = adContainer.querySelector('video');
+        if (adVideo && adVideo.duration) {
+            muteAndSpeedUp(adVideo, 8.0);
+            try {
+                adVideo.currentTime = adVideo.duration;
+            } catch (e) {
+                // Uncaught TypeError for ad duration, doesn't affect anything
+            }
+        }
+
+        // The Skip button is no longer clickable
+        const skipButton = document.querySelector(skipButtonSelectors.join(', '));
+        if (skipButton) {
+            // There is a skip button at the ad end screen, speed up the timer
+            adContainer.playbackRate = 16.0;
+        }
     }
 
     function muteAndSpeedUp(videoElement, playbackRate) {
